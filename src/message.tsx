90ce44374d51369cb191c95399c56dff9ff67d19
fe-u <1214517669@qq.com>
2023-08-05 17:34:40 +0800

    initial

---
+++ src/message.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.scss';
+
+const Message = () => {
+  return <div className="message-container">message-12</div>;
+};
+
+export default Message;
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'ts-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'message component',
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    open: true,
+    hot: true,
+    port: 3000,
+  },
+};
